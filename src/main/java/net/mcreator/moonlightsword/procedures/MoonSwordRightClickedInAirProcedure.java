945e162f87e5b266cfdac4ab8a2ce26ee6b75564
GoogFB <91232659+GoogFB@users.noreply.github.com>
2021-11-04 10:55:33 -0700

    i broke minecraft

--- src/main/java/net/mcreator/moonlightsword/procedures/MoonSwordRightClickedInAirProcedure.java
+++ src/main/java/net/mcreator/moonlightsword/procedures/MoonSwordRightClickedInAirProcedure.java
@@ -7,6 +7,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.Explosion;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.entity.projectile.FireballEntity;
@@ -17,82 +18,97 @@ import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.Entity;
 
+import net.mcreator.moonlightsword.MoonlightswordModVariables;
 import net.mcreator.moonlightsword.MoonlightswordMod;
 
 import java.util.Map;
 
 public class MoonSwordRightClickedInAirProcedure {
-	public static void executeProcedure(Map<String, Object> dependencies){
-			if(dependencies.get("entity") == null) {
-				if(!dependencies.containsKey("entity"))
-					MoonlightswordMod.LOGGER.warn("Failed to load dependency entity for procedure MoonSwordRightClickedInAir!");
-				return;
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				MoonlightswordMod.LOGGER.warn("Failed to load dependency entity for procedure MoonSwordRightClickedInAir!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				MoonlightswordMod.LOGGER.warn("Failed to load dependency x for procedure MoonSwordRightClickedInAir!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				MoonlightswordMod.LOGGER.warn("Failed to load dependency y for procedure MoonSwordRightClickedInAir!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				MoonlightswordMod.LOGGER.warn("Failed to load dependency z for procedure MoonSwordRightClickedInAir!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MoonlightswordMod.LOGGER.warn("Failed to load dependency world for procedure MoonSwordRightClickedInAir!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		double speed = 0;
+		double yaw = 0;
+		speed = (double) 0.1;
+		yaw = (double) (entity.rotationYaw);
+		if ((MoonlightswordModVariables.MapVariables.get(world).projectile == (false))) {
+			MoonlightswordModVariables.MapVariables.get(world).projectile = (boolean) (true);
+			MoonlightswordModVariables.MapVariables.get(world).syncData(world);
+			if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+				((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("pewpew"), (true));
 			}
-			if(dependencies.get("x") == null) {
-				if(!dependencies.containsKey("x"))
-					MoonlightswordMod.LOGGER.warn("Failed to load dependency x for procedure MoonSwordRightClickedInAir!");
-				return;
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new FireballEntity(EntityType.FIREBALL, (World) world);
+				entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
+				entityToSpawn.setRenderYawOffset((float) 0);
+				entityToSpawn.setRotationYawHead((float) 0);
+				entityToSpawn.setMotion((speed * Math.cos(((yaw + 90) * (Math.PI / 180)))), ((entity.getMotion().getY()) + 0.2),
+						(speed * Math.sin(((yaw + 90) * (Math.PI / 180)))));
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
 			}
-			if(dependencies.get("y") == null) {
-				if(!dependencies.containsKey("y"))
-					MoonlightswordMod.LOGGER.warn("Failed to load dependency y for procedure MoonSwordRightClickedInAir!");
-				return;
+			if (world instanceof World && !((World) world).isRemote) {
+				((World) world).createExplosion(null, (int) x, (int) y, (int) z, (float) 900, Explosion.Mode.BREAK);
 			}
-			if(dependencies.get("z") == null) {
-				if(!dependencies.containsKey("z"))
-					MoonlightswordMod.LOGGER.warn("Failed to load dependency z for procedure MoonSwordRightClickedInAir!");
-				return;
+			new Object() {
+				private int ticks = 0;
+				private float waitTicks;
+				private IWorld world;
+				public void start(IWorld world, int waitTicks) {
+					this.waitTicks = waitTicks;
+					MinecraftForge.EVENT_BUS.register(this);
+					this.world = world;
+				}
+
+				@SubscribeEvent
+				public void tick(TickEvent.ServerTickEvent event) {
+					if (event.phase == TickEvent.Phase.END) {
+						this.ticks += 1;
+						if (this.ticks >= this.waitTicks)
+							run();
+					}
+				}
+
+				private void run() {
+					MoonlightswordModVariables.MapVariables.get(world).projectile = (boolean) (false);
+					MoonlightswordModVariables.MapVariables.get(world).syncData(world);
+					MinecraftForge.EVENT_BUS.unregister(this);
+				}
+			}.start(world, (int) 100);
+		} else {
+			if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+				((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("pewpew ready"), (true));
 			}
-			if(dependencies.get("world") == null) {
-				if(!dependencies.containsKey("world"))
-					MoonlightswordMod.LOGGER.warn("Failed to load dependency world for procedure MoonSwordRightClickedInAir!");
-				return;
-			}
-				Entity entity = (Entity) dependencies.get("entity");
-				double x = dependencies.get("x") instanceof Integer
-					? (int) dependencies.get("x") : (double) dependencies.get("x");
-				double y = dependencies.get("y") instanceof Integer
-					? (int) dependencies.get("y") : (double) dependencies.get("y");
-				double z = dependencies.get("z") instanceof Integer
-					? (int) dependencies.get("z") : (double) dependencies.get("z");
-				IWorld world = (IWorld) dependencies.get("world");
-double speed = 0;double yaw = 0;
-		speed = (double) 0.1;yaw = (double) (entity.rotationYaw);if ((==(false))) {if(entity instanceof PlayerEntity && !entity.world.isRemote()){
-	((PlayerEntity)entity).sendStatusMessage(new StringTextComponent("FireBall Ability Activated"), (true));
-}	if(world instanceof ServerWorld) {
-			Entity entityToSpawn = new FireballEntity(EntityType.FIREBALL, (World) world);
-		entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
-		entityToSpawn.setRenderYawOffset((float) 0);
-		entityToSpawn.setRotationYawHead((float) 0);
-		entityToSpawn.setMotion((speed*Math.cos(((yaw+90)*(Math.PI/180)))),((entity.getMotion().getY())+0.2),(speed*Math.sin(((yaw+90)*(Math.PI/180)))));
-		if (entityToSpawn instanceof MobEntity)
-			((MobEntity)entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()), SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
-		world.addEntity(entityToSpawn);
-	}
-new Object() {
-    private int ticks = 0;
-    private float waitTicks;
-    private IWorld world;
-    public void start(IWorld world, int waitTicks) {
-		this.waitTicks = waitTicks;
-		MinecraftForge.EVENT_BUS.register(this);
-		this.world = world;
-	}
-    @SubscribeEvent
-    public void tick(TickEvent.ServerTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            this.ticks += 1;
-            if (this.ticks >= this.waitTicks)
-                run();
-        }
-    }
-    private void run() {
-        
-        MinecraftForge.EVENT_BUS.unregister(this);
-    }
-}.start(world, (int) 100);
-}else{if(entity instanceof PlayerEntity && !entity.world.isRemote()){
-	((PlayerEntity)entity).sendStatusMessage(new StringTextComponent("FireBall Ability is on Cooldown"), (true));
-}}
+		}
 	}
 }
